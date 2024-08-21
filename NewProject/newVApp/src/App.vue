a7d9f88b2996caae1a87cbfdd9c52597850d0760
Charan Romi <charanromi325@gmail.com>
2024-08-21 10:00:08 +0530

    first commit

---
+++ NewProject/newVApp/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <div class="hero">
+           <div class="navbar">
+               <div class="newlinks">
+                   <ul>
+                       <li><router-link to="/"><i class="fa fa-home"></i> Home</router-link></li>
+                       <li><router-link to="/courses"><i class="fa fa-university"></i> Courses</router-link></li>
+                       <li><router-link to="/faq"><i class="fa fa-quora"></i> FAQ</router-link></li>
+                       <li><router-link to="/about"><i class="fa fa-user"></i> About Us</router-link></li>
+                       <li><router-link to="/contact"><i class="fa fa-phone"></i> Contact Us</router-link></li>
+                       <li><button>Sign In</button></li>
+                   </ul>
+               </div>  
+           </div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #333;
+    color: white;
+    padding: 10px 20px;
+}
+
+.newlinks {
+    margin-left: auto;
+}
+
+.newlinks ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+.newlinks ul li {
+    display: inline;
+    margin-right: 10px;
+}
+
+.newlinks ul li a {
+    text-decoration: none;
+    color: white;
+}
+
+.newlinks ul li button {
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    cursor: pointer;
+}
+
+.newlinks ul li button:hover {
+    background-color: #a04554;
+}
+
+</style>
---
+++ NewProject/newVApp/src/components/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="home">
+    <div class="container">
+          <h1>Open University of Sri Lanka</h1><br><br>
+          <p><span>The Open University of Sri Lanka (OUSL)</span> is a national university in Sri Lanka. It is unique within the Sri Lankan national university system for being the only university to offer programs of study leading to Certificate, Diploma, Degrees and Postgraduate degrees up to PhD level through the <span>Open and Distance Mode of Learning(ODL).</span> 
+        The degrees awarded by the university are treated as equivalent to degrees awarded by any other Sri Lankan University under the preview of the University Grants Commission.</p>
+          <br>
+          <h3>Courses offered</h3>
+          <ol>
+           <li>Software Engineering</li>
+           <li>Computer Science</li>
+           <li>Networking</li>
+          </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.container{
+    margin-top: 15px;
+    display: block;
+}
+
+h1{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    text-align: center;
+    text-decoration: none;
+    color: #4CAF50;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+p{
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: 22px;
+    font-family: sans-serif;
+
+}
+
+h3{
+    font-family: cursive;
+    font-style: inherit;
+    color: #a04554;
+}
+
+ol{
+    font-size: 18px;
+    color: rgb(61, 107, 55);
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    
+}
+
+span{
+    font-weight: bold;
+}
+</style>
